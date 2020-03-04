fe628752c9b6b144d3607e0ed6cb463b423d5d87
Serge Modin <modinsv@yandex.ru>
2020-03-04 22:24:22 +0300

    Add Logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "./types";
 
 //export const getLogs = () => {
 //return async (dispatch) => {
@@ -14,12 +14,12 @@ import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
 //}
 //}
 
-//Get logs from server
+// Get logs from server
 export const getLogs = () => async dispatch => {
   try {
     setLoading();
 
-    const res = await fetch("/logs");
+    const res = await fetch('/logs');
     const data = await res.json();
 
     dispatch({
@@ -29,7 +29,33 @@ export const getLogs = () => async dispatch => {
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.data
+      payload: err.response.statusText
+    });
+  }
+};
+
+// Add new log
+export const addLog = log => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch('/logs', {
+      method: 'POST',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_LOG,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.statusText
     });
   }
 };
